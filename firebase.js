cb74fb288ba4bf269ad57f9a72ce7a840e98beae
Emmanuella Orioma <oriomaemmanuella@gmail.com>
2024-08-14 20:50:09 +0100

    added firebase config

--- firebase.js
+++ firebase.js
@@ -1,15 +1,25 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+// import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
- };
+    apiKey: "AIzaSyAUH3xVvvirFvI_g98f8dGRgD6VCizCLmE",
+    authDomain: "thepantryapp-b9a35.firebaseapp.com",
+    projectId: "thepantryapp-b9a35",
+    storageBucket: "thepantryapp-b9a35.appspot.com",
+    messagingSenderId: "229490784987",
+    appId: "1:229490784987:web:2f2911cabd117b29562afe"
+  };
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 const firestore = getFirestore(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
